32674c9bbd5db8a3689e70285826fc1a084370f3
YOURS-PC\YOURS <peiwencao@vip.qq.com>
2017-10-25 18:30:06 +0800

    开环的调试程序，带无PWM的指令控制函数

--- 程序代码/CarChassis/CarChassis.c
+++ 程序代码/CarChassis/CarChassis.c
@@ -1,8 +1,55 @@
 #include<reg51.h>
 #define uchar unsigned char
 #define uint unsigned int
-	
-void delay_ms(unsigned int x)
+
+//lefa
+sbit lefa1 = P2^1;
+sbit lefa2 = P2^2;
+sbit lefap = P2^0;
+//lefb 2
+sbit lefb1 = P2^3;
+sbit lefb2 = P2^4;
+sbit lefbp = P2^5;
+//riga 1
+sbit riga1 = P1^1;
+sbit riga2 = P1^2;
+sbit rigap = P1^0;
+//rigb 2
+sbit rigb1 = P1^3;
+sbit rigb2 = P1^4;
+sbit rigbp = P1^5;
+
+void ctrlm(uchar motorn,uchar dire)
+{
+	//motorn 0:lefta 1:leftb 2:righta 3:rightb
+	//dire means 1:goback  2:go  other:stop
+	bit out1,out2,outp;
+	out1=1;out2=1;outp=1;
+	switch(dire)
+	{
+		case 0:
+			out1=1;out2=1;outp=1;break;
+		case 1:
+			out1=1;out2=0;outp=1;break;
+		case 2:
+			out1=0;out2=1;outp=1;break;
+		default:
+			out1=1;out2=1;outp=1;break;
+	}
+	switch(motorn)
+	{
+		case 0:
+			lefa1=out1;lefa2=out2;lefap=outp;break;
+		case 1:
+			lefb1=out1;lefb2=out2;lefbp=outp;break;
+		case 2:
+			riga1=out1;riga2=out2;rigap=outp;break;
+		case 3:
+			rigb1=out1;rigb2=out2;rigbp=outp;break;
+	}
+}
+
+void delay(unsigned int x)
 {
 	uchar t;
 	while(x--)
@@ -13,15 +60,26 @@ void delay_ms(unsigned int x)
 
 void main()
 {
-	P2=0xFE;
-	delay_ms(500);
+	P1=0xFF;
+	P2=0xFF;
+	delay(1000);
 	while(1)
 	{
-		P2=0xFE;
-		delay_ms(500);
-		P2=0xFD;
-		delay_ms(500);
-		P2=0xFB;
-		delay_ms(500);
+		ctrlm(0,1);
+		delay(2000);
+		ctrlm(1,1);
+		delay(2000);
+		ctrlm(2,1);
+		delay(2000);
+		ctrlm(3,1);
+		delay(2000);
+		ctrlm(0,0);
+		delay(2000);
+		ctrlm(1,0);
+		delay(2000);
+		ctrlm(2,0);
+		delay(2000);
+		ctrlm(3,0);
+		delay(4000);
 	}
 }
