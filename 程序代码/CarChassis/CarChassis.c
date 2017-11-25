be3ffafc5fe3998be330c06d9678470f48eee3b1
YOURSC-PC\YOURS <peiwencao@vip.qq.com>
2017-11-25 20:02:34 +0800

    修改CarChassis.c

--- 程序代码/CarChassis/CarChassis.c
+++ 程序代码/CarChassis/CarChassis.c
@@ -1,6 +1,13 @@
 #include<reg51.h>
+#include<intrins.h>
+
 #define uchar unsigned char
-#define uint unsigned int
+#define int8 short
+#define int8u unsigned short
+#define int16 int
+#define int16u unsigned int
+#define int32 long
+#define int32u unsigned long
 
 sbit motor_a1 = P0^0;
 sbit motor_a2 = P0^1;
@@ -14,6 +21,13 @@ sbit motor_ap = P2^0;
 sbit motor_bp = P2^1;
 sbit motor_cp = P2^2;
 sbit motor_dp = P2^3;
+
+//设定值
+static int8 pwm0=10;
+
+//全局变量
+int8 pwma0=0,pwmb0=0,pwmc0=0,pwmd0=0;
+int8 pwma,pwmb,pwmc,pwmd,pwm=0;
 
 void ctrlm(uchar motor,uchar dire)
 {
@@ -31,7 +45,6 @@ void ctrlm(uchar motor,uchar dire)
 		default:
 			data1=0;data2=0;break;
 	}
-	
 	switch(motor)
 	{
 		case 'a':
@@ -47,38 +60,60 @@ void ctrlm(uchar motor,uchar dire)
 	}
 }
 
-void delay(unsigned int x)
+void delayms(unsigned int t)
 {
-	uchar t;
-	while(x--)
+	int16u i,j;
+	for(i=0;i<t;i++)
 	{
-		for(t=0;t<120;t++);
+		for(j=0;j<=100;j++)
+		{
+			_nop_();
+		}
 	}
+}
+
+void InitTimer0(void)
+{
+	TMOD=0x01;
+	TH0=0x0FC;
+	TL0=0x18;
+	EA=1;
+	ET0=1;
+	TR0=1;
+}
+
+void Timer0Interrupt(void) interrupt 1 using 3
+{
+	TH0=0x0FC;
+	TL0=0x18;
+	pwma--;
+	pwmb--;
+	pwmc--;
+	pwmd--;
 }
 
 void main()
 {
 	P1=0xFF;
 	P0=0xFF;
-	delay(1000);
+	delayms(1000);
 	while(1)
 	{
-		ctrlm('a',1);
-		delay(2000);
-		ctrlm('b',1);
-		delay(2000);
-		ctrlm('c',1);
-		delay(2000);
-		ctrlm('d',1);
-		delay(2000);
-		ctrlm('a',3);
-		delay(2000);
-		ctrlm('b',3);
-		delay(2000);
-		ctrlm('c',3);
-		delay(2000);
-		ctrlm('d',3);
-		delay(2000);
-		P0=0xFF;
+		if(pwm<=0)
+		{
+			pwm=pwm0;
+			motor_ap=0;
+			motor_bp=0;
+			motor_cp=0;
+			motor_dp=0;
+			pwma=pwma0;
+			pwmb=pwmb0;
+			pwmc=pwmc0;
+			pwmd=pwmd0;
+		}
+		if(pwma<=0) motor_ap=1;
+		if(pwmb<=0) motor_bp=1;
+		if(pwmc<=0) motor_cp=1;
+		if(pwmd<=0) motor_dp=1;
 	}
 }
