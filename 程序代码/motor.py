96e9cfe01d49cd75885f625d23ff8f4d4372f119
YOURSC-PC\YOURS <peiwencao@vip.qq.com>
2017-12-08 16:41:46 +0800

    新功能：Xbox360手柄的按键控制小车

--- 程序代码/motor.py
+++ 程序代码/motor.py
@@ -4,113 +4,113 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-# 定义连线端口
-RA1 = 22
-RA2 = 27
-RB1 = 24
-RB2 = 23
-LA1 = 6
-LA2 = 5
-LB1 = 21
-LB2 = 20
-PWMRA = 17
-PWMRB = 18
-PWMLA = 13
-PWMLB = 12
-Frequency = 200
-#PWM = int(sys.argv[1])
-PWM = 50
-#delay = int(sys.argv[2])
-
-class motor(object):
+class car(object):
+    def __init__(self):
+        # 定义连线端口
+        self.RA1 = 22
+        self.RA2 = 27
+        self.RB1 = 24
+        self.RB2 = 23
+        self.LA1 = 6
+        self.LA2 = 5
+        self.LB1 = 21
+        self.LB2 = 20
+        self.PWMRA = 17
+        self.PWMRB = 18
+        self.PWMLA = 13
+        self.PWMLB = 12
+        self.Frequency = 200
+        self.PWM = 50
     def GPIO_INIT(self):
         # 初始化PIN口
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(RA1,GPIO.OUT)
-        GPIO.setup(RA2,GPIO.OUT)
-        GPIO.setup(RB1,GPIO.OUT)
-        GPIO.setup(RB2,GPIO.OUT)
-        GPIO.setup(LA1,GPIO.OUT)
-        GPIO.setup(LA2,GPIO.OUT)
-        GPIO.setup(LB1,GPIO.OUT)
-        GPIO.setup(LB2,GPIO.OUT)
-        GPIO.setup(PWMRA,GPIO.OUT)
-        GPIO.setup(PWMRB,GPIO.OUT)
-        GPIO.setup(PWMLA,GPIO.OUT)
-        GPIO.setup(PWMLB,GPIO.OUT)
+        GPIO.setup(self.RA1,GPIO.OUT)
+        GPIO.setup(self.RA2,GPIO.OUT)
+        GPIO.setup(self.RB1,GPIO.OUT)
+        GPIO.setup(self.RB2,GPIO.OUT)
+        GPIO.setup(self.LA1,GPIO.OUT)
+        GPIO.setup(self.LA2,GPIO.OUT)
+        GPIO.setup(self.LB1,GPIO.OUT)
+        GPIO.setup(self.LB2,GPIO.OUT)
+        GPIO.setup(self.PWMRA,GPIO.OUT)
+        GPIO.setup(self.PWMRB,GPIO.OUT)
+        GPIO.setup(self.PWMLA,GPIO.OUT)
+        GPIO.setup(self.PWMLB,GPIO.OUT)
         # 初始化PWM
-        self.RA = GPIO.PWM(PWMRA,Frequency)
-        self.RA.start('0')
-        self.RB = GPIO.PWM(PWMRB,Frequency)
+        self.RA = GPIO.PWM(self.PWMRA,self.Frequency)
+        self.RA.start(0)
+        self.RB = GPIO.PWM(self.PWMRB,self.Frequency)
         self.RB.start(0)
-        self.LA = GPIO.PWM(PWMLA,Frequency)
+        self.LA = GPIO.PWM(self.PWMLA,self.Frequency)
         self.LA.start(0)
-        self.LB = GPIO.PWM(PWMLB,Frequency)
+        self.LB = GPIO.PWM(self.PWMLB,self.Frequency)
         self.LB.start(0)
-    def WHEEL(wheel,pwm):
-        if pwm==0:
-            self.motor1=GPIO.LOW
-            self.motor2=GPIO.LOW
-            self.motorp=0
-        elif pwm>=0:
-            self.motor1=GPIO.HIGH
-            self.motor2=GPIO.LOW
-            self.motorp=pwm
-        else:
-            self.motor1=GPIO.LOW
-            self.motor2=GPIO.HIGH
-            self.motorp=-pwm
+    def WHEEL(self,wheel,pwm):
+        # 选择控制口
+        if abs(pwm)>=100:
+            pwm = int(100*(pwm/abs(pwm)))
+        motorp = abs(pwm)
         if wheel=='RA':
-            GPIO.output(RA1,self.motor1)
-            GPIO.output(RA2,self.motor2)
-            self.RA.ChangeDutyCycle(self.motorp)
+            motor1=self.RA1
+            motor2=self.RA2
+            self.RA.ChangeDutyCycle(motorp)
         elif wheel=='RB':
-            GPIO.output(RA1,self.motor1)
-            GPIO.output(RA2,self.motor2)
-            self.RB.ChangeDutyCycle(self.motorp)
+            motor1=self.RB1
+            motor2=self.RB2
+            self.RA.ChangeDutyCycle(motorp)
         elif wheel=='LA':
-            GPIO.output(RA1,self.motor1)
-            GPIO.output(RA2,self.motor2)
-            self.LA.ChangeDutyCycle(self.motorp)
+            motor1=self.LA1
+            motor2=self.LA2
+            self.RA.ChangeDutyCycle(motorp)
         elif wheel=='LB':
-            GPIO.output(RA1,self.motor1)
-            GPIO.output(RA2,self.motor2)
-            self.LB.ChangeDutyCycle(self.motorp)
+            motor1=self.LB1
+            motor2=self.LB2
+            self.RA.ChangeDutyCycle(motorp)
+        # 控制字输出
+        if pwm==0:
+            GPIO.output(motor1,GPIO.LOW)
+            GPIO.output(motor2,GPIO.LOW)
+        elif pwm>=0:
+            GPIO.output(motor1,GPIO.HIGH)
+            GPIO.output(motor2,GPIO.LOW)
+        else:
+            GPIO.output(motor1,GPIO.LOW)
+            GPIO.output(motor2,GPIO.HIGH)
     def UP(self):
-        self.WHEEL('RA',PWM)
-        self.WHEEL('RB',PWM)
-        self.WHEEL('LA',PWM)
-        self.WHEEL('LB',PWM)
+        self.WHEEL('RA',self.PWM)
+        self.WHEEL('RB',self.PWM)
+        self.WHEEL('LA',self.PWM)
+        self.WHEEL('LB',self.PWM)
         print('up')
     def DOWN(self):
-        self.WHEEL('RA',-PWM)
-        self.WHEEL('RB',-PWM)
-        self.WHEEL('LA',-PWM)
-        self.WHEEL('LB',-PWM)
+        self.WHEEL('RA',-self.PWM)
+        self.WHEEL('RB',-self.PWM)
+        self.WHEEL('LA',-self.PWM)
+        self.WHEEL('LB',-self.PWM)
         print('down')
-    def LEFT(self):
-        self.WHEEL('RA',PWM)
-        self.WHEEL('RB',-PWM)
-        self.WHEEL('LA',-PWM)
-        self.WHEEL('LB',PWM)
-        print('left')
     def RIGHT(self):
-        self.WHEEL('RA',-PWM)
-        self.WHEEL('RB',PWM)
-        self.WHEEL('LA',PWM)
-        self.WHEEL('LB',-PWM)
+        self.WHEEL('RA',-self.PWM)
+        self.WHEEL('RB',self.PWM)
+        self.WHEEL('LA',self.PWM)
+        self.WHEEL('LB',-self.PWM)
         print('right')
+    def LEFT(self):
+        self.WHEEL('RA',self.PWM)
+        self.WHEEL('RB',-self.PWM)
+        self.WHEEL('LA',-self.PWM)
+        self.WHEEL('LB',self.PWM)
+        print('left')
     def SHUN(self):
-        self.WHEEL('RA',-PWM)
-        self.WHEEL('RB',-PWM)
-        self.WHEEL('LA',PWM)
-        self.WHEEL('LB',PWM)
+        self.WHEEL('RA',-self.PWM)
+        self.WHEEL('RB',-self.PWM)
+        self.WHEEL('LA',self.PWM)
+        self.WHEEL('LB',self.PWM)
         print('shun')
     def NI(self):
-        self.WHEEL('RA',PWM)
-        self.WHEEL('RB',PWM)
-        self.WHEEL('LA',-PWM)
-        self.WHEEL('LB',-PWM)
+        self.WHEEL('RA',self.PWM)
+        self.WHEEL('RB',self.PWM)
+        self.WHEEL('LA',-self.PWM)
+        self.WHEEL('LB',-self.PWM)
         print('ni')
     def STOP(self):
         self.WHEEL('RA',0)
@@ -118,10 +118,6 @@ class motor(object):
         self.WHEEL('LA',0)
         self.WHEEL('LB',0)
         print('stop')
-    def END(self):
-        GPIO.cleanup
-        print('end')
-
-        
-
-
+#    def END(self):
+#        GPIO.cleanup
+#		print('end')
--- 程序代码/xbox360.py
+++ 程序代码/xbox360.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-
+# 作者：曹沛文
 ########################################
 ## 运行于树莓派，需要第三方库的支持   ##
 ## sudo apt install joystick xboxdrv  ##
@@ -10,18 +10,19 @@
 
 from evdev import InputDevice
 from select import select
+# 小车电机驱动自建库
 import motor
 
+car = motor.car()
 dev = InputDevice('/dev/input/event0')
-mctrl = motor.motor()
 
 while True:
     r,w,x = select([dev], [], [])
     for event in dev.read():
-#        print(event)
         incode = event.code
         inval  = event.value
         print('==============')
+        print(event)
         # tpye int3
         if incode==0:
             print('LT')
@@ -39,17 +40,17 @@ while True:
         elif incode==16:
             if inval==1:
                 print('Right')
-                mctrl.RIGHT()
+                car.RIGHT()
             elif inval==-1:
                 print('Left')
-                mctrl.LEFT()
+                car.LEFT()
         elif incode==17:
             if inval==-1:
                 print('Up')
-                mctrl.UP()
+                car.UP()
             elif inval==1:
                 print('Down')
-                mctrl.DOWN()
+                car.DOWN()
         elif incode==304:
             print('A')
         elif incode==305:
@@ -60,18 +61,18 @@ while True:
             print('Y')
         elif incode==314:
             print('SELET')
-            mctrl.STOP()
+            car.STOP()
         elif incode==315:
             print('START')
-            mctrl.GPIO_INIT()
+            car.GPIO_INIT()
         elif incode==316:
             print('HOME')
         elif incode==310:
             print('L1')
-            mctrl.NI()
+            car.NI()
         elif incode==311:
             print('R1')
-            mctrl.SHUN()
+            car.SHUN()
         elif incode==317:
             print('L0')
         elif incode==318:
@@ -81,5 +82,6 @@ while True:
             print(incode)
         print(inval)
         if inval==0:
-            mctrl.STOP
+            pass
+            #car.STOP()
         break
