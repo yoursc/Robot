4e3877caaf473faaf1d15f858c71028f5eb19df9
YOURSC-PC\YOURS <peiwencao@vip.qq.com>
2018-05-09 23:53:10 +0800

    开发：增加串口发送功能

--- Code/Pi_ROS/catkin_ws/src/Ultrasonic_wave/scripts/go.py
+++ Code/Pi_ROS/catkin_ws/src/Ultrasonic_wave/scripts/go.py
@@ -10,19 +10,32 @@ import serial
 from  ultrasound_wave.msg import Distance
 
 # 变量设置
-VG = 16 #前进速度
-VT = 8  #转向速度
-F_Stop = 10 # cm 前方停车阈值
+VG = 24     # 前进速度
+VT = 16     # 转向速度
+F_Stop = 8  # cm 前方停车阈值
 S_Stop = 8  # cm 侧方停车阈值
-F_Slow = 50 # cm 前方减速阈值
-S_Slow = 30 # cm 侧方加速阈值
+F_Slow = 30 # cm 前方减速阈值
+S_Slow = 20 # cm 侧方加速阈值
 
-def tranhex(num):
-    a = hex(num)
-    a = a.strip('0x')
-    if len(a)==1:
-        a = '0' + a
-        return a
+def hexs(num):
+    a = hex(abs(num))
+    # 转换为hex值
+    if len(a)==4:
+        b = a[2]+ a[3]
+    else:
+        b = "0" + a[2]
+    return b
+
+def ctrl(wheel_l,wheel_r):
+    LL,RR="ff","ff"
+    if wheel_l<0:
+        LL = "00"
+    if wheel_r<0:
+        RR = "00"
+    data = "fffe"+hexs(wheel_l)+LL+hexs(wheel_r)+RR
+    data = data.decode("hex")
+    ser.write(data)
+
 def callback(data):
     V = VG
     TL,TR,T = 0,0,0
@@ -45,9 +58,18 @@ def callback(data):
     elif data_r<=S_Slow:
         TR = VT * (S_Slow - data_r)/float(S_Slow - S_Stop)
     # 汇总处理
-    T = TL - TR
+    T = TR - TL
     VL = int(V - T)
     VR = int(V + T)
+    ctrl(VL,VR)
+#    LL,RR="ff","ff"
+#    if VL<0:
+#        LL = "00"
+#    if VR<0:
+#        RR = "00"
+#    outd = "fffe"+hexs(VL)+LL+hexs(VR)+RR
+#    outd = outd.decode("hex")
+#    s = ser.write(outd)
     # 显示计算后原始信息
     #rospy.loginfo("V:%2s  T:%2s  VL:%2.3f  VR:%2.3f ",V,T,TL,TR)
     # 显示两电机设定值
@@ -57,7 +79,9 @@ def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('chatter', Distance, callback)
     rospy.spin()
+    ctrl(0,0)
 
 if __name__ == '__main__':
+    ser = serial.Serial("/dev/ttyUSB0",115200,timeout=1)
     listener()
 
